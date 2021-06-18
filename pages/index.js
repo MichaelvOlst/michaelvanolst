6a55ef552b5c0c56402151ecbaf48166b71f36ea
Michael van Olst <michaeltje@live.nl>
2021-06-18 21:55:10 +0200

    edited the about me section

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import { vs2015 } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 
 export default function Home() {
 
+   const dateOfBirth = new Date('1989 09 16 GMT');
+
   const codeString = `
   <?php
   
@@ -13,7 +15,16 @@ export default function Home() {
 
       public function aboutMe() : string
       {
-          return "Hello it's Michael";
+          return "
+          Hello 👋,
+          I am Michael van Olst, ${dateOfBirth.getTime()} years old and living in the Netherlands.
+          I am a PHP Developer with 5 years of relevant work experience and love to learn new things. 
+          Over the years I have gained knowledge and experience in: PHP, Laravel, OOP, Reactjs, MySQL,
+          JavaScript, HTML, CSS, Linux, XML, Git and a bit of GoLang.
+          Currently I am working for CM Specialist in Huizen where I am responsible for the backend for their in-house made CMS.
+          I integrated a lot API's with the CMS e.g. Payment providers like Mollie, 
+          Enterprise Resource Planning like Exact Online and Snelstart and Transporters like DHL, UPS and Sendcloud.  
+          ";
       }
 
 
@@ -22,7 +33,9 @@ export default function Home() {
           return [
             new Skill('PHP'),
             new Skill('MySql'),
+            new Skill('Laravel'),
             new Skill('JavaScript'),
+            new Skill('Next.js'),
           ];
       }
 
