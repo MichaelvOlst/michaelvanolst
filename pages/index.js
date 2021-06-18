8d3a7ce355ce751fdbcd44569fbd8f9bbe81085d
Michael van Olst <michaeltje@live.nl>
2021-06-18 23:16:49 +0200

    added an icon for code

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout'
+import { FaCode } from 'react-icons/fa';
+
 export default function Home() {
-
-
   return (
     <Layout>
       
@@ -18,7 +18,7 @@ export default function Home() {
           <div className="mx-auto space-x-6 ">
             <h1 className="text-white text-4xl">Michael van Olst</h1>
             <Link href="/resume"><a className="text-white pt-2 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:transform-none">Regular resume</a></Link>
-            <Link href="/code"><a className="text-white pt-2 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:transform-none">Code resume</a></Link>
+            <Link href="/code"><a className="text-white pt-2 transition transform hover:-translate-y-1 motion-reduce:transition-none motion-reduce:transform-none">Code <FaCode className="inline-block"/></a></Link>
           </div>
         </div>
       </div>
